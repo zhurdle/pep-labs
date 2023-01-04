f0d8ca47350a0cde587bed1f4d3738ea70c55b05
zhurdle <hurdlezachary@gmail.com>
2023-01-04 17:25:56 +0000

    Saved progress on SetAllArrayElementsToN 2023-01-04 17:25:56 +0000

--- SetAllArrayElementsToN/src/main/java/SetAllArrayElements.java
+++ SetAllArrayElementsToN/src/main/java/SetAllArrayElements.java
@@ -1,4 +1,3 @@
-
 public class SetAllArrayElements {
     /**
      * Your task: you will recieve an array and a number, and you will have to set every element of the array to that
@@ -23,6 +22,9 @@ public class SetAllArrayElements {
      * @return nothing, because of pass-by-reference, all changes will be reflected in the original array.
      */
     public void set(int[] arr, int n){
-
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = n;
+        }
+        System.out.println(arr);
     }
 }
