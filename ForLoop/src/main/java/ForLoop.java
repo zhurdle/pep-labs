bc4a46d071bce8efa10a79f792d5b6e380071677
zhurdle <hurdlezachary@gmail.com>
2023-01-04 12:13:29 +0000

    Saved progress on ForLoop 2023-01-04 12:13:29 +0000

--- ForLoop/src/main/java/ForLoop.java
+++ ForLoop/src/main/java/ForLoop.java
@@ -42,7 +42,10 @@ public class ForLoop {
     public String returnNumbers(int start, int end){
         String buildingString = "";
 
-//        code here
+//        for(int i = start; i < end;  i++){
+            buildingString = buildingString + i + " ";
+            
+}
 
         return buildingString;
     }
