3f8ae71836226a54a53939b258b91f3e22238c3d
zhurdle <hurdlezachary@gmail.com>
2023-01-04 18:28:54 +0000

    Saved progress on ReturnArrayContents 2023-01-04 18:28:54 +0000

--- ReturnArrayContents/src/main/java/ReturnArrayContents.java
+++ ReturnArrayContents/src/main/java/ReturnArrayContents.java
@@ -1,3 +1,5 @@
+import javax.net.ssl.SSLEngineResult;
+
 public class ReturnArrayContents {
     /**
      * Return a string with an array's contents, formatted with spaces.
@@ -28,6 +30,13 @@ public class ReturnArrayContents {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        for(int i = 0; i < nums.length ; i++){
+            if (i < nums.length) {
+                return " ";
+            } else {
+                return String.valueOf(nums[i]);
+            }
+        }
+        return String.valueOf(nums[i]);
     }
 }
